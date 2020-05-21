cc2955d5f43c15d3230c86b0e06617fa3a9e90a9
Nicktoms <ntomassian@gmail.com>
2020-05-20 22:29:47 -0300

    JSON-Categories
    Creación JSON Categories con SubCategories
    Tratamiento en Vista

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,6 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const categoriesFilePath = path.join(__dirname, '../data/categories.json');
+const categoriesJSON = JSON.parse(fs.readFileSync(categoriesFilePath, 'utf-8'));
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const productsJSON = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const categoriesController = {
-    categories: (req,res) => {
-        res.render('categories');
+    categories: (req, res) => {
+        res.render('categories', {
+            categoriesJSON,
+            productsJSON
+        }
+        );
     }
 };
 
