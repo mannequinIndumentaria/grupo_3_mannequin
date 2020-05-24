19acd091a9332f6ef0faa170472a243942d1dfc8
Nicktoms <ntomassian@gmail.com>
2020-05-24 20:19:30 -0300

    Actualización Index, Categories, Imágenes

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const categoriesFilePath = path.join(__dirname, '../data/categories.json');
+const categoriesJSON = JSON.parse(fs.readFileSync(categoriesFilePath, 'utf-8'));
+
 const indexController = {
     index: (req,res) => {
-        res.render('index');
+        res.render('index', {
+            categoriesJSON
+        });
     }
 };
 
