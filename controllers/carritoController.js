f8b4b9b405a303e61dee4cb68c58d080839cb468
earca <earca@maxisistemas.com.ar>
2020-05-24 21:43:11 -0300

    edite el nombre de las imagenes

--- controllers/carritoController.js
+++ controllers/carritoController.js
@@ -1,7 +1,21 @@
+// const arrayProductos = []; //products.json
 const carritoController = {
     carrito: (req,res) => {
         res.render('carrito');
-    }
+    },
+
+    // filter: (req,res) => {
+    //     const categoria = req.params.categoria;
+    //     const subcategoria = req.params.subcategoria;
+    //     const desde = req.params.desde;
+    //     const hasta = req.params.hasta;    
+    //     const articulosFiltrados = arrayProductos.filter(articulo =>{
+    //         return articulo.category == categoria && articulo.subcategory == subcategoria;
+    //     });
+    //     const productosEnPagina = articulosFiltrados.slice(desde,hasta);
+    //     res.render('carrito',{data: productosEnPagina});
+    // }
+
 };
 
 module.exports = carritoController;
