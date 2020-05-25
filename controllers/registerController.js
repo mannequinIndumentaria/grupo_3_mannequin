1f1184fd657be068a6dbaa20009838c22bf7b175
Maria Clara Visconti <claravisconti@hotmail.com>
2020-05-25 06:12:02 -0300

    login/register

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -7,17 +7,19 @@ const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 
 const registerController = {
+    /*Render-get login/main-register*/
     register: (req,res) => {
         res.render('register');
     },
 
+    /*Render-get register-form*/
     registro: (req,res) => {
         res.render('registerFormCompleto');
     },
 
     /*Register: Almacenar nuevo usuario*/
     store: (req, res, next) => {
-        console.log(req.body);
+        //console.log(req.body);
         const newUser = {
             id: users[users.length - 1].id + 1,
             name: req.body.name,
@@ -28,6 +30,29 @@ const registerController = {
 
         const userToSave = [...users, newUser];
         fs.writeFileSync(usersFilePath, JSON.stringify(userToSave, null, ' '));
+        res.redirect('/');
+    },
+
+    /*Login: Almacenar nuevo usuario*/
+    login: (req,res,next) => {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const usuario = users.find((user) => {
+            return user.email == email;
+        });
+
+        if (!usuario) {
+            res.render('register', {
+                error: 'Usuario no encontrado!'
+            });
+        }  
+        if(!bcrypt.compareSync(password, usuario.password)) {
+            res.render('register', {
+                error: 'Password incorrecto!'
+            });
+        }
+
         res.redirect('/');
     }
 };
