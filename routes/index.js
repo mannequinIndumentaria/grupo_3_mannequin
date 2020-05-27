7344dc0749780c35f04d1b53d2c5426f822cc074
Maria Clara Visconti <claravisconti@hotmail.com>
2020-05-27 20:19:31 -0300

    carr

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const indexController = require('../controllers/indexController')
 
 /* GET home page. */
+
 router.get('/', indexController.index);
 
 module.exports = router;
