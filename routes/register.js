7354b2d6fef8edb1fc5ae8db2de90fe50b59206b
Maria Clara Visconti <claravisconti@hotmail.com>
2020-05-28 02:15:59 -0300

    login
    login

--- routes/register.js
+++ routes/register.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 const registerController = require('../controllers/registerController')
 
-/*log-in. */
-router.get('/', registerController.login);
+/*log-in*/
+router.get('/', registerController.index);
+router.post('/', registerController.login);
 
 /*Register*/
 router.get('/registro', registerController.registro);
