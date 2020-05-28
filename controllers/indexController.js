4d269641ff9f026ac9b81891bc78b462abbcaadc
Maria Clara Visconti <claravisconti@hotmail.com>
2020-05-27 23:35:40 -0300

    carr
    carr2

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -11,32 +11,53 @@ const productsJSON = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const productsInfoFilePath = path.join(__dirname, '../data/products-info.json');
 const productsInfoJSON = JSON.parse(fs.readFileSync(productsInfoFilePath, 'utf-8'));
 
-/*Filtro products por Nueva Temporada y Sale*/
-const productosNewSeason = productsJSON.filter(pdto => pdto.new_season == true);
-const productosSale = productsJSON.filter(pdto => pdto.sale == true);
-
 const indexController = {
-    index: (req,res) => {
-            /*Array vacio*/
-            const productos = [];
+    index: (req, res) => {
+
+        /*New Season*/
+
+        const productosNewSeason = productsJSON.filter(pdto => pdto.new_season == true);
+
+        const productosNS = [];
+        
         for (const item of productosNewSeason) {
-            /*Buscar imagen*/
-            const imgItem = productsInfoJSON.filter( element => element.product_id == item.id)
-            /*Crear nuevo array para almecenar propiedades puntuales*/
+            const imgItem = productsInfoJSON.filter(element => element.product_id == item.id)
             const productosNewArray = {
-                id : item.id,
-                name : item.name,
-                price : item.price,
-                img : imgItem[0].images[0]
+                id: item.id,
+                name: item.name,
+                price: item.price,
+                img: imgItem[0].images[0]
             }
-            console.log(imgItem[0].images[0]);
-        productos.push(productosNewArray);
+            productosNS.push(productosNewArray);
+        };
+
+        /*Sale*/
+
+        const productosSale = productsJSON.filter(pdto => pdto.sale == true);
+
+        const productosS = [];
+
+        for (const itemB of productosSale) {
+            const imgItemB = productsInfoJSON.filter(element => element.product_id == itemB.id)
+            const productosNewArrayB = {
+                id: itemB.id,
+                name: itemB.name,
+                price: itemB.price,
+                img: imgItemB[0].images[0]
+            }
+            productosS.push(productosNewArrayB);
         }
+
+        /*Info del controlador a vista*/
+
         res.render('index', {
             categoriesJSON,
-            productosNewSeason: productos            
+            productosNewSeason: productosNS,
+            productosSale: productosS
+
         });
     }
+
 };
 
 module.exports = indexController;
