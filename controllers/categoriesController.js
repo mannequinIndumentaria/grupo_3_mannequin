77529377b4b90a4a245e1ca2eca32c51a2add300
Nicktoms <ntomassian@gmail.com>
2020-05-27 22:02:58 -0300

    Actualización-Controller

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -6,6 +6,8 @@ const productsFilePath = path.join(__dirname, '../data/products.json');
 const productsJSON = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const productsInfoFilePath = path.join(__dirname, '../data/products-info.json');
 const productsInfoJSON = JSON.parse(fs.readFileSync(productsInfoFilePath, 'utf-8'));
+const productsColorPath = path.join(__dirname, '../data/colors.json');
+const productsColorJSON = JSON.parse(fs.readFileSync(productsColorPath, 'utf-8'));
 
 
 const categoriesController = {
@@ -24,35 +26,44 @@ const categoriesController = {
         const to = (from + 7);
         productsFilteredCat = productsJSON.filter(article => {
             return category == article.category;
-
         });
         productsFilteredSub = productsFilteredCat.filter(subcat => {
             return subcat.subcategory == subcategory
         });
         const productsOnSite = productsFilteredSub.slice(from, to);
 
-        productsTest = productsFilteredSub
+        const productsFinal = [];
+        for (product of productsOnSite) {
+            let productImgColor = productsInfoJSON.filter(element => {
+                return element.product_id == product.id
+            })
+            imageArray = [];
+            colorArray = [];
+            for (const colors of productImgColor) {
+                let color = productsColorJSON.filter(element => {
+                    return element.id == colors.color_id
+                })
+                colorArray.push(color);
 
-        let imagesOnSite = (productsTest, productsInfoJSON) => {
-            for (product of productsTest) {
-                for (prodInfo of productsInfoJSON) {
-                    console.log(prodInfo);
-                    if (productsTest.id == productsInfoJSON.product_id) {
-                        imagesOnSite.color = productsInfoJSON.color_id,
-                            imagesOnSite.image = productsInfoJSON.images[0]
-                    }
-                }
+                imageArray.push(colors.images[0]);
             }
 
-            return imagesOnSite;
-
+            const producto = {
+                id: product.id,
+                name: product.name,
+                price: product.price,
+                colors: colorArray,
+                image: imageArray
+            }
+            productsFinal.push(producto);
         }
-        console.log(imagesOnSite);
+        console.log(productsFinal)
+
+
         res.render('categories',
             {
                 categoriesJSON,
-                productsOnSite: productsOnSite,
-                imagesOnSite: imagesOnSite
+                productsOnSite: productsFinal
             });
 
     }
