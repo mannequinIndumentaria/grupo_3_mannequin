4f955663d887be51973f91d1d9abc7fddf0e3168
Maria Clara Visconti <claravisconti@hotmail.com>
2020-06-10 12:39:25 -0300

    form PUT funcionando
    form PUT funcionando

--- routes/profile.js
+++ routes/profile.js
@@ -7,8 +7,10 @@ const usersFilePath = path.join(__dirname, '../data/users.json');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 const { check, validationResult, body } = require('express-validator');
 
-/* GET users listing. */
-router.get('/', profileController.index);
+/* Vista profile */
+router.get('/:userId', profileController.index);
+/*Modificar formulario*/
+router.put('/:userId', profileController.update);
 
 module.exports = router;
 
