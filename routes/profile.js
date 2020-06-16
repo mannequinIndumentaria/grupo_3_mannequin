cbee3b0e8daff008d0ced63891b5aaab1073c84f
earca <earca@maxisistemas.com.ar>
2020-06-16 02:38:33 -0300

    fixes varios
    Se obtienen datos de los select.
    Se implementa multer para la subida de imagenes.
    Se corrige error en update con campo email.
    Se corrige error en update al guardar info(se implementa bcrypt)

--- routes/profile.js
+++ routes/profile.js
@@ -6,11 +6,22 @@ const profileController = require('../controllers/profileController');
 const usersFilePath = path.join(__dirname, '../data/users.json');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 const { check, validationResult, body } = require('express-validator');
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+cb(null,'public/images/users')
+    },
+    filename: function(req,file,cb){
+        cb(null, file.fieldname+'-'+ Date.now() + path.extname(file.originalname))
+    },
+});
+
+const upload = multer({storage: storage});
 
 /* Vista profile */
 router.get('/:userId', profileController.index);
 /*Modificar formulario*/
-router.put('/:userId', profileController.update);
+router.put('/:userId', upload.any(), profileController.update);
 
 module.exports = router;
 
