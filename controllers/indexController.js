5b1b3aeec1bf94b906e889780ea3e408c0b7ef30
Nicktoms <ntomassian@gmail.com>
2020-07-11 02:41:38 -0300

    pagination-products
    Agregado de robustez sobre URL req.query, paginado mejorado.
    Agregado de Order by para todas los tipos de listado de productos.
    -Pendientes de resolver -
    Enlaces pagination search
    Breadcrumb con desfasaje

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -65,6 +65,56 @@ const indexController = {
     },
     /*Search*/
     search: async (req, res) => {
+        let order = {
+            query: [],
+            url: 'name'
+        }
+
+        switch (req.query.order) {
+            case 'name':
+                order.query = [['name', 'ASC']]
+                order.url = 'name'
+                break;
+            case 'price':
+                order.query = [['price', 'ASC']]
+                order.url = 'price'
+                break;
+            case 'color':
+                order.query = [['color', 'ASC']]
+                order.url = 'color'
+                break;
+            default:
+                order.query = [['name', 'ASC']]
+                order.url = 'name'
+        }
+        //Atrapo si no viene query param de page
+        var page_nu = 0
+        function pagina() {
+            if (req.query.page) { page_nu = parseInt(req.query.page) - 1 }
+            return page_nu
+        }
+        page_nu = pagina()
+
+        //Atrapo si no viene query param de page
+        var tam_nu = 5
+        function tamano() {
+            if (req.query.size) { tam_nu = parseInt(req.query.size) }
+
+            return tam_nu
+        }
+        tam_nu = tamano()
+        console.log(tam_nu)
+
+        const pagination = {
+            page_number: page_nu,
+            page_size: tam_nu,
+            total_products: 0,
+            pages: 0,
+            filtered: 2,
+            keyword: req.query.keywords,
+            order: order.url
+
+        };
 
         const finalSearch = await db.Product.findAndCountAll({
             where: {
@@ -75,9 +125,15 @@ const indexController = {
             include: [
                 { association: "images" }, { association: "sizes" }
             ],
-            distinct: true
+            limit: pagination.page_size,
+            offset: pagination.page_size * pagination.page_number,
+            distinct: true,
+            order: order.query
 
         });
+
+        pagination.total_products = finalSearch.count
+        pagination.pages = Math.ceil(pagination.total_products / pagination.page_size)
 
         /*let finalSearch = productsJSON.filter(prod => prod.name.toLowerCase().includes(userSearch.toLowerCase()) ? prod : null);
         
@@ -108,13 +164,7 @@ const indexController = {
             pagination.pages = Math.ceil(productsColorFoto.length / pagination.page_size);
             productsFinal = productsColorFoto.slice((pagination.page_number - 1) * pagination.page_size, pagination.page_number * pagination.page_size);
 */
-        const pagination = {
-            page_number: 0,
-            page_size: 5,
-            total_products: finalSearch.count,
-            filtered: 2
 
-        };
 
 
 
