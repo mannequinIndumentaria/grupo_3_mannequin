bfdecf0cc1529005d011d51345d93a855843e8e3
Nicktoms <ntomassian@gmail.com>
2020-08-08 22:16:09 -0300

    Fetch y página FAQ

--- controllers/api/subscribersController.js
+++ controllers/api/subscribersController.js
@@ -7,9 +7,26 @@ const subscribersController = {
             active: 1
         };
 
-        db.Subscriber.create(newSubscriber);
-        console.log(newSubscriber);
+        const querySuscribers = await db.Subscriber.findAll({
+            where: {
+                email: newSubscriber.email
+            }
+        })
 
+        if (querySuscribers.length == 0) {
+
+            db.Subscriber.create(newSubscriber);
+            res.json({
+                status: 201,
+                url: '/api/subscribers'
+            })
+            console.log(newSubscriber);
+        } else {
+            res.json({
+                status: 403,
+                url: '/api/subscribers'
+            })
+        }
 
     }
 }
---
+++ scripts/subscribe.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', function () {
+    var subscribeBtn = document.querySelector(".subscribeBtn")
+    var avisoSuscripto = document.querySelector(".aviso-suscrpito")
+    var submitSubsc = subscribeBtn.addEventListener("click", function (e) {
+        e.preventDefault()
+        fetch('http://localhost:3000/api/subscribers', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Contant-Type': 'application/json'
+            }
+        })
+            .then(function (res) {
+                if (res.status == 201) {
+                    avisoSuscripto.innerHTML = "¡Se ha suscrito a nuestro email!"
+                }
+            })
+            .catch(error => console.error('Error:', error))
+            .then(function (res) {
+                console.log('Suscrito: ', res)
+            })
+    })
+})
