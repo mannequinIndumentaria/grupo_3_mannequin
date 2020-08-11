af0d5103aefba114fed7fe60e5e743f4ae689a27
Nicktoms <ntomassian@gmail.com>
2020-08-10 21:39:42 -0300

    Api-subscribers + Página Privacy

--- controllers/api/subscribersController.js
+++ controllers/api/subscribersController.js
@@ -1,19 +1,20 @@
 const db = require('../../database/models');
 
 const subscribersController = {
-    subscribe: async (req, res, next) => {
+    subscribe: async (req, res) => {
         const newSubscriber = {
             email: req.body.email,
             active: 1
         };
-
-        const querySuscribers = await db.Subscriber.findAll({
+        console.log("Mail Back", req.body)
+        const querySuscribers = await db.Subscriber.findOne({
             where: {
                 email: newSubscriber.email
             }
         })
+        console.log(querySuscribers)
 
-        if (querySuscribers.length == 0) {
+        if (querySuscribers == null) {
 
             db.Subscriber.create(newSubscriber);
             res.json({
