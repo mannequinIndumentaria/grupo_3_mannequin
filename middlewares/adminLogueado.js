ca7a77144d62c0d4b0a2e5b337228c5b11eddfd0
earca <earca@maxisistemas.com.ar>
2020-08-12 12:18:29 -0300

    fixes varios
    Se implementa middleware para CRUD
    Fix de menu sin subcategoria en index
    Se agregan opciones de dashboard y HOME en crud
    Se reemplaza la cabeza loca, por un fondo en SVG con el texto ADMIN.

--- middlewares/adminLogueado.js
+++ middlewares/adminLogueado.js
@@ -5,6 +5,7 @@ const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 const db = require('../database/models');
 
 async function usuarioLogueado(req, res, next) {
+    console.log("ADMIN-MIDDLEWARE")
     if (req.session.user == undefined) {
         if (req.cookies.user) {
             const usuario = await db.User.findAll(
@@ -17,22 +18,20 @@ async function usuarioLogueado(req, res, next) {
             // const usuario = users.find((user) => {
             //     return user.id == req.cookies.user;
             // });
-            req.session.user = usuario;
-            next();
+            console.log("esadmin", usuario.admin)
+            if(usuario.admin == 1){
+
+                req.session.user = usuario;
+                next();
+            }else{
+                res.redirect('/index')
+            }
         }else{
             // no hay session ni cookies
             res.redirect('/register');
         }
     }else{
-        // hay sesion
-        // Validar que sea el usuario
-        // console.log("parametro",req.params.userId)
-        // console.log("sesion",req.session.user.idusers)
-        if(req.params.userId == req.session.user.idusers){
-            next();
-        }else{
-            res.redirect('/register');
-        }
+        next();
     }
 }
 
