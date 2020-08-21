c7ec0483306d6216ebb82c939eddfbb697c32021
earca <earca@maxisistemas.com.ar>
2020-08-21 10:43:25 -0300

    Update products.js

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const apiProductsController = require('../../controllers/api/ProductsController')
+const apiProductsController = require('../../controllers/api/productsController')
 
 /* GET home page. */
 router.get('/', apiProductsController.getAllProducts);
