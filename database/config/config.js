bb6ff4c50694c9ab15a5ac33336f1f062498a26d
earca <earca@maxisistemas.com.ar>
2020-08-20 21:18:26 -0300

    Update config.js

--- database/config/config.js
+++ database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "mannequin_db",
-    "host": "127.0.0.1",
+    "username": "uh1a6uvcgk5udvl0",
+    "password": "prxatyf9w5x50t5f",
+    "database": "agg0cyxdqvsqgqc0",
+    "host": "u28rhuskh0x5paau.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
     
   },
