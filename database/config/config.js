d8b1437d1b6cce26bbb2ca15b220d08dc68685f5
earca <earca@maxisistemas.com.ar>
2020-08-21 10:15:28 -0300

    update config

--- database/config/config.js
+++ database/config/config.js
@@ -1,11 +1,10 @@
 module.exports = {
   "development": {
-    "username": "uh1a6uvcgk5udvl0",
-    "password": "prxatyf9w5x50t5f",
-    "database": "agg0cyxdqvsqgqc0",
-    "host": "u28rhuskh0x5paau.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "username": "root",
+    "password": null,
+    "database": "mannequin_db",
+    "host": "127.0.0.1",
     "dialect": "mysql"
-    
   },
   "test": {
     "username": "root",
@@ -16,11 +15,10 @@ module.exports = {
     
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "mannequin_db",
-    "host": "127.0.0.1",
+    "username": "uh1a6uvcgk5udvl0",
+    "password": "prxatyf9w5x50t5f",
+    "database": "agg0cyxdqvsqgqc0",
+    "host": "u28rhuskh0x5paau.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
-    
   }
 }
