4ab5f41ff7a3a54cdfd2961ea2ff7cd2552824ae
earca <earca@maxisistemas.com.ar>
2020-08-21 16:01:57 -0300

    fix query

--- controllers/api/categoriesController.js
+++ controllers/api/categoriesController.js
@@ -20,7 +20,7 @@ const categoryController = {
         const respuesta = await db.sequelize.query(`SELECT name, ifnull(total,0) as total_productos,count(name) as total_categorias FROM (select pc.parent,count(p.idproducts) as total from products p
         inner join product_categories pc ON pc.idproduct_categories = p.product_categories_idproduct_categories
         group by pc.parent) as Suma
-        right join product_categories pc1 ON pc1.idproduct_categories = suma.parent
+        right join product_categories pc1 ON pc1.idproduct_categories = Suma.parent
         where pc1.parent IS null
         group by name`)
         
